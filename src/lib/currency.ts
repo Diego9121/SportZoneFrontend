c2510300e240b953fe78b858d5c214d846487e39
Diego <drm.e2104@gmail.com>
2026-06-30 23:47:30 -0400

    cambio de moneda

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -3,3 +3,14 @@ export const SIMBOLO_MONEDA = 'Bs.'
 export function formatMoneda(valor: number): string {
   return `${SIMBOLO_MONEDA} ${valor.toFixed(2)}`
 }
+
+
+
+export function formatMonedaCard(valor: number): string {
+  const numero = new Intl.NumberFormat('es-BO', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(valor)
+
+  return `${SIMBOLO_MONEDA} ${numero}`
+}
